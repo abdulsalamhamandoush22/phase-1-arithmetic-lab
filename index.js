5bfc22d67129052529e6d6853cdd9785872b63d2
abdulsalamhamandoush22 <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-04-16 14:10:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2; 
+console.log(multiply); 
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1; 
+console.log(random);
+
+let num3 = 26;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let Numbers = [5, 20, 3, 12, 8, 7];
+let max = Math.max(Numbers); 
+console.log(max);
